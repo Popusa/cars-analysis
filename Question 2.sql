bd8c503f6c0c84d717d7f8ad2247c2a1f4997c98
Danii <danielmaged.3@gmail.com>
2025-03-29 03:20:05 +0200

    refined q2 answer

--- Question 2.sql
+++ Question 2.sql
@@ -1,9 +1,24 @@
-SELECT 
+WITH split_kilo AS(
+select
 	kilometers,
-    AVG(price_thousands)
-FROM
+	SUBSTRING_INDEX(SUBSTRING_INDEX(kilometers, ' to ', 1), ' ', -1) as initial,
+	SUBSTRING_INDEX(SUBSTRING_INDEX(kilometers, ' to ', 2), ' ', -1) as final,
+    price_thousands
+from
 	cars_dev
+)
+
+SELECT
+	ceil(((initial + final) / 2)) as avg_kilometers,
+    AVG(price_thousands) as avg_price
+FROM
+	split_kilo
 GROUP BY
-	Kilometers
+	avg_kilometers
 ORDER BY
-	AVG(price_thousands) DESC;
+	avg_price DESC;
+    
+-- As per Query, there is a slight correlation between milleage going up, and and prices going down.
+
+-- Result not conclusive.
+    
